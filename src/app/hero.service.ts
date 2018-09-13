9cd5677e26744f51fd1bff54e227e579448cd251
Carlos Arturo Cabrera Camacho - Ceiba Software <carlos.cabrera@ceiba.com.co>
2018-09-13 14:25:21 -0500

    Finalize the routing section of the Heroes angular tutorial

---
+++ src/app/hero.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HEROES} from './mock-heroes';
+import {Hero} from './hero';
+import {Observable, of} from 'rxjs';
+import {MessageService} from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+  constructor(private messageService: MessageService) { }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`); 
+    return of(HEROES.find(hero => hero.id ===id));
+  }
+}
